8ddb97b95274f8134444e92347b21a12f4f423ba
Neil Houghton <neil@nizmow.net>
2020-06-10 11:09:02 +1000

    Add support for decimals as slyly specified, and negatives just in case

--- Application.Tests/NumberToEnglishService_Tests.cs
+++ Application.Tests/NumberToEnglishService_Tests.cs
@@ -1,13 +1,16 @@
-using System;
-using Application.Domain;
+using System.Runtime.InteropServices;
+using Application.Services;
 using FluentAssertions;
 using Xunit;
 
 namespace Application.Tests
 {
-    public class UnitTest1
+    public class NumberToEnglishService_Tests
     {
         [Theory]
+        [InlineData(-283, "negative two hundred and eighty three")]
+        [InlineData(-11.25, "negative eleven point two five")]
+        [InlineData(0.32023, "zero point three two zero two three")]
         [InlineData(0, "zero")]
         [InlineData(8, "eight")]
         [InlineData(37, "thirty seven")]
@@ -15,11 +18,13 @@ namespace Application.Tests
         [InlineData(422, "four hundred and twenty two")]
         [InlineData(2367, "two thousand three hundred and sixty seven")]
         [InlineData(23000, "twenty three thousand")]
-        [InlineData(43012, "forty three thousand and twelve")]
+        [InlineData(43000.123, "forty three thousand point one two three")]
+        [InlineData(59012, "fifty nine thousand and twelve")]
         [InlineData(70129, "seventy thousand one hundred and twenty nine")]
         [InlineData(7836197, "seven million eight hundred and thirty six thousand one hundred and ninety seven")]
         [InlineData(256000000, "two hundred and fifty six million")]
-        public void Test1(int input, string expectedResult)
+        [InlineData(83013123001, "eighty three billion thirteen million one hundred and twenty three thousand and one")]
+        public void NumberToInput_WithValidNumber_Succeeds(decimal input, string expectedResult)
         {
             var target = new NumberToEnglishService();
 
---
+++ Application/Services/NumberToEnglishService.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Application.Services
+{
+    public class NumberToEnglishService
+    {
+        private readonly string[] numbersBelowTwentyMap;
+        private readonly Dictionary<int, string> tensMap;
+
+        public NumberToEnglishService()
+        {
+            this.numbersBelowTwentyMap = new[]
+            {
+                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+                "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+            };
+
+            this.tensMap = new Dictionary<int, string>
+            {
+                { 2, "twenty" },
+                { 3, "thirty" },
+                { 4, "forty" },
+                { 5, "fifty" },
+                { 6, "sixty" },
+                { 7, "seventy" },
+                { 8, "eighty" },
+                { 9, "ninety" },
+            };
+        }
+
+        public string NumberToEnglish(decimal number)
+        {
+            var sb = new StringBuilder();
+
+            var wholeNumberPart = (Int64)Decimal.Truncate(number);
+            var decimalPart = number - wholeNumberPart;
+            if (wholeNumberPart < 0)
+            {
+                sb.Append("negative ");
+                wholeNumberPart = Math.Abs(wholeNumberPart);
+                decimalPart = Math.Abs(decimalPart);
+            }
+
+            sb.Append(this.WholeNumberToEnglish(wholeNumberPart, true));
+
+            if (decimalPart > 0)
+            {
+                sb.Append(" point ");
+                sb.Append(this.DecimalToEnglish(decimalPart));
+            }
+
+            return sb.ToString();
+        }
+
+        private string DecimalToEnglish(decimal number)
+        {
+            if (number > 1)
+            {
+                throw new ArgumentException("argument must be less than 1", nameof(number));
+            }
+
+            // there's probably a more efficient way to do this than converting things back and forth between strings
+            // and chars and ints (memory allocs!) but let's do the naive straightforward approach, surround with
+            // tests and come back to optimise later (maybe with benchmarks).
+
+            var numberString = number.ToString(CultureInfo.InvariantCulture);
+            numberString = numberString.Substring(2, numberString.Length - 2);
+
+            var sb = new StringBuilder();
+            foreach (var character in numberString)
+            {
+                var digit = int.Parse(character.ToString());
+                sb.Append(this.numbersBelowTwentyMap[digit]);
+                sb.Append(" ");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        private string WholeNumberToEnglish(Int64 number, bool firstCall = false)
+        {
+            // TODO: a bit of DRY in here checking remainders, be nice to tidy it up
+
+            var sb = new StringBuilder();
+
+            if (number >= 1000000000)
+            {
+                sb.Append($"{this.NumberToEnglish(number / 1000000000)} billion");
+                var remainder = number % 1000000000;
+                if (remainder == 0)
+                {
+                    return sb.ToString();
+                }
+
+                if (remainder < 1000)
+                {
+                    sb.Append(" and");
+                }
+
+                sb.Append($" {this.NumberToEnglish(remainder)}");
+                return sb.ToString();
+            }
+
+            if (number / 1000000 >= 1)
+            {
+                sb.Append($"{this.WholeNumberToEnglish(number / 1000000)} million");
+                var remainder = number % 1000000;
+                if (remainder == 0)
+                {
+                    return sb.ToString();
+                }
+
+                if (remainder < 1000)
+                {
+                    sb.Append(" and");
+                }
+
+                sb.Append($" {this.WholeNumberToEnglish(remainder)}");
+                return sb.ToString();
+            }
+
+            if (number / 1000 >= 1)
+            {
+                sb.Append($"{this.WholeNumberToEnglish(number / 1000)} thousand");
+                var remainder = number % 1000;
+                if (remainder == 0)
+                {
+                    return sb.ToString();
+                }
+
+                if (remainder < 100)
+                {
+                    sb.Append(" and");
+                }
+
+                sb.Append($" {this.WholeNumberToEnglish(remainder)}");
+
+                return sb.ToString();
+            }
+
+            if (number / 100 >= 1)
+            {
+                sb.Append($"{this.WholeNumberToEnglish(number / 100)} hundred");
+                var remainder = number % 100;
+                if (remainder == 0)
+                {
+                    // do not recurse
+                    return sb.ToString();
+                }
+
+                sb.Append($" and {this.WholeNumberToEnglish(remainder)}");
+
+                return sb.ToString();
+            }
+
+            if (number / 10 >= 2)
+            {
+                sb.Append(this.tensMap[(int)(number / 10)]);
+                var remainder = number % 10;
+                
+                // WARNING: throwing away less than 1 here for simplicity!
+                if (remainder < 1)
+                {
+                    return sb.ToString();
+                }
+
+                sb.Append($" {this.WholeNumberToEnglish(number % 10)}");
+            }
+
+            if (number < 20)
+            {
+                if (!firstCall && number == 0)
+                {
+                    return string.Empty;
+                }
+
+                // cast is safe, we're checking size of the decimal
+                sb.Append(this.numbersBelowTwentyMap[(int)number]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
